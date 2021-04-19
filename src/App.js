7da5995b6c8d01e8df1790a78498a5d0ff995acb
valdcarl <77943334+valdcarl@users.noreply.github.com>
2021-04-19 01:14:59 -0600

    test1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react;'
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import './App.css';
 import MovieList from "./components/MovieList";
