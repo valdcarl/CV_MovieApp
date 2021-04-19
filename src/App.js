8bc40d30add100bb0eae7031e020d2066b5f9429
valdcarl <77943334+valdcarl@users.noreply.github.com>
2021-04-19 00:46:21 -0600

    test1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react;'
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 import './App.css';
+import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
-function App() {
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMovieRequest = async () => {
+    const url = `http://www.omdbapi.com/?s=?{searchValue}&apikey=3dca0b0f`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if(responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={'container-fluid movie-app'}>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
+      <div className='row'>
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MovieList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MovieList = (props) => {
+    return (
+        <>
+            {props.movies.map((movie, index) => (
+                <div className='image-container d-flex justify-content-start m-3'>
+                    <img src={movie.Poster} alt='movie'></img>
+                    <div className="overlay f-flex align-items-center justify-content-center"></div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default MovieList;
